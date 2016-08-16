59c43ac973c390f8587097c052e28265dfd8a212
Mickael Burguet <mickael.burguet@ssense.com>
2016-08-16 16:46:58 -0400

    Add typescript definitions

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export namespace TestRailSyncWrapper {
+    export class ScenarioSynchronizer {
+        constructor(options: any);
+    }
+
+    export class ResultSynchronizer {
+        constructor(options: any);
+        createNewTestRun(callback: Function): void;
+        pushResult(scenario: any, callback: Function): void;
+    }
+
+    export function readConfig(): void;
+}
