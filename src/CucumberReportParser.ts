597ca12a9332fd3366acae862bece80dfa758fd7
dropdbs <dropdbs@gmail.com>
2020-03-17 18:31:45 +0100

    update lint

--- src/CucumberReportParser.ts
+++ src/CucumberReportParser.ts
@@ -1,40 +1,40 @@
 import { Scenario, TestCaseReport, Step } from './index';
 
 export class CucumberReportParser {
-    protected report: Array<any>;
-    protected scenarios: Array<Scenario>;
+  protected report: any[];
+  protected scenarios: Scenario[];
 
-    public parseReport(report: Array<any>): Array<Scenario> {
-        this.report = report;
-        this.scenarios = [];
-        report.forEach((result: any) => {
-            this.parseTestCases(result.elements);
-        });
-        return this.scenarios;
-    }
+  public parseReport(report: TestCaseReport[]): Scenario[] {
+    this.report = report;
+    this.scenarios = [];
+    report.forEach((result: any) => {
+      this.parseTestCases(result.elements);
+    });
+    return this.scenarios;
+  }
 
-    protected parseTestCases(testCases: Array<any>): any {
-        testCases.forEach((testCase: TestCaseReport) => {
-            const error = this.parseTestCase(testCase);
-            const scenario = <Scenario> {
-                tags: [testCase.tags[0].name],
-                isPending: false,
-                isUndefined: false,
-                isSkipped: false,
-                isSuccessful: error === null,
-                exception: error
-            };
-            this.scenarios.push(scenario);
-        });
-    }
+  protected parseTestCases(testCases: TestCaseReport[]): void {
+    testCases.forEach((testCase: TestCaseReport) => {
+      const error = this.parseTestCase(testCase);
+      const scenario = <Scenario> {
+        tags: [testCase.tags[0].name],
+        isPending: false,
+        isUndefined: false,
+        isSkipped: false,
+        isSuccessful: error === null,
+        exception: error
+      };
+      this.scenarios.push(scenario);
+    });
+  }
 
-    protected parseTestCase(testCase: any): Error {
-        let error = null;
-        testCase.steps.forEach((step: Step) => {
-            if (step.result.status !== 'passed') {
-                error = `status[${step.result.status}] step[${step.name}]`;
-            }
-        });
-        return error;
-    }
+  protected parseTestCase(testCase: TestCaseReport): Error {
+    let error = null;
+    testCase.steps.forEach((step: Step) => {
+      if (step.result.status !== 'passed') {
+        error = `status[${step.result.status}] step[${step.name}]`;
+      }
+    });
+    return error;
+  }
 }
--- src/bin/push-test-result-file.ts
+++ src/bin/push-test-result-file.ts
@@ -2,46 +2,37 @@
 import * as program from 'commander';
 import * as path from 'path';
 import { ResultSynchronizer, readConfig, CucumberReportParser } from '../index';
+import * as fs from 'fs';
 
 const parser = new CucumberReportParser();
 
-program
-    .option('--file <String>', 'filename in results directory')
-    .parse(process.argv);
+program.option('--file <String>', 'filename in results directory').parse(process.argv);
 
 const error = (s: string): void => {
-    program.outputHelp();
-    console.log();
-    console.log('Error: ' + s);
-    process.exit(1);
+  program.outputHelp();
+  console.log();
+  console.log('Error: ' + s);
+  process.exit(1);
 };
 
 if (!(<any> program).file || [0, NaN].indexOf((<any> program).file) !== -1) {
-    error('<testcase file> is required and must be a string');
+  error('<testcase file> is required and must be a string');
 }
 const config = readConfig();
 config.pushResults = true;
 
 const resultFile = String((<any> program).file);
+
 let results;
 try {
-    results = require(path.resolve(config.resultsDir, resultFile));
-} catch (e) {
-    error(
-        `<testcase file> error reading file[${path.resolve(
-            config.resultsDir,
-            resultFile
-        )}] exception[${e}]`
-    );
+  const jsonString = fs.readFileSync(path.resolve(config.resultsDir, resultFile));
+  results = JSON.parse(String(jsonString));
+} catch (err) {
+  error(`<testcase file> error reading file[${path.resolve(config.resultsDir, resultFile)}] exception[${err}]`);
 }
 
 if (!Array.isArray(results)) {
-    error(
-        `<testcase file> wrong cucumber format result file ${path.resolve(
-            config.resultsDir,
-            resultFile
-        )}`
-    );
+  error(`<testcase file> wrong cucumber format result file ${path.resolve(config.resultsDir, resultFile)}`);
 }
 
 const scenarios = parser.parseReport(results);
@@ -49,14 +40,8 @@ const scenarios = parser.parseReport(results);
 const sync = new ResultSynchronizer(config);
 const savePromises = [];
 for (const scenario of scenarios) {
-    savePromises.push(sync.saveTestResult(scenario));
+  savePromises.push(sync.saveTestResult(scenario));
 }
 Promise.all(savePromises)
-    .then(() =>
-        sync.readRemoteTestRuns().then(() =>
-            sync.pushTestResults().then(function() {
-                console.log(`sync done`);
-            })
-        )
-    )
-    .catch(e => error(e));
+  .then(() => sync.readRemoteTestRuns().then(() => sync.pushTestResults().then(() => console.log('sync done'))))
+  .catch(e => error(e));
